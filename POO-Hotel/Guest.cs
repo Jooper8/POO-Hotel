733dabc1e2de1450a782c60e4461a3893712e647
Frederico Proença <joopereight@gmail.com>
2021-10-06 23:28:05 -0300

    Add project files.

---
+++ POO-Hotel/Guest.cs
@@ -0,0 +1,29 @@
+﻿//Frederico Guilherme Camilli Proença RA:200925
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Hotel
+{
+    class Guest
+    {
+        //Declaração das auto properties Name e Email.
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //Declaração do construtor Guest, que possui como parametros as variáveis name e email, que se baseiam nas autoproperties já declaradas.
+        public Guest(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        //Superclasse que será retornada no programa principal, retornando o Name e Email.
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
---
+++ POO-Hotel/Program.cs
@@ -0,0 +1,47 @@
+﻿//Frederico Guilherme Camilli Proença RA:200925
+using System;
+
+namespace POO_Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração do vetor Guest baseado na classe Guest.
+            Guest[] vect = new Guest[10];
+            //Declaração das variáveis string name e email e da int room, que serão depois preenchidas pelo usuário.
+            string name, email;
+            int room;
+
+            //Inserção do número de quartos a serem alugados.
+            Console.WriteLine("Insert the number of rooms to be rented:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.WriteLine();
+                //Inserção do nome, e-mail e número de quarto de cada hóspede cujo quarto será alugado.
+                Console.WriteLine($"Guest #{i}");
+                Console.WriteLine("Name");
+                name = Console.ReadLine();
+                Console.WriteLine("Email");
+                email = Console.ReadLine();
+                Console.WriteLine("Room");
+                room = int.Parse(Console.ReadLine());
+                //Instanciação do vetor room, que instancia os itens name e email da classe Guest.
+                vect[room] = new Guest(name, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Rented rooms: ");
+            //Exposição dos dados inseridos pelo usuário ao usuário, sendo exibidos de forma ascendente baseado no número dos quartos.
+            for (int i = 0; i < 10; i ++)
+
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
